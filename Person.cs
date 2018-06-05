925546f239b8257b373f2fe97777b26cf1620b94
lfortes <fortesl@yahoo.com>
2018-06-05 17:32:22 -0500

    initial commit

---
+++ Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrokenMediator
+{
+    class Person
+    {
+        public string Name;
+        public ChatRoom Room;
+        private List<string> chatlog = new List<string>();
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void Say(string message)
+        {
+            Room.Broadcast(Name, message);
+        }
+
+        public void PrivateMessage(string who, string message)
+        {
+            Room.Message(Name, who, message);
+        }
+
+        public void Receive(string sender, string message)
+        {
+            string s = $"{sender}: '{message}";
+            chatlog.Add(s);
+            Console.WriteLine($"[{Name}'s chat session] {s}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace BrokenMediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var room = new ChatRoom();
+
+            var john = new Person("John");
+            room.Join(john);
+
+            var jane = new Person("Jane");
+            room.Join(jane);
+
+            john.Say("Hi Jane");
+            jane.Say("Hi John, how are you?");
+
+            var simon = new Person("Simon");
+            room.Join(simon);
+
+            simon.Say("Hi everyone");
+            jane.PrivateMessage("Simon", "Glad you could join us");
+        }
+    }
+}
